bbb868a93fbba83c18ca35457e953d5abfb3832c
Brayan Calvo <brayancalvo64@gmail.com>
2024-09-24 22:32:04 -0500

    Actualizacion manipulacion de DOM y anexo ataque de enemigo aleatorio

--- script/script.js
+++ script/script.js
@@ -6,27 +6,43 @@ function iniciarJuego() {
 function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById("Hipodoge")
     let inputCapipero = document.getElementById("Capipero")
-    let inputRatigueya = document.getElementById("Ratigueya")
-    let inputLangostelvis = document.getElementById("Langostelvis")
-    let inputTucapalma = document.getElementById("Tucapalma")
     let inputPydos = document.getElementById("Pydos")
+    let spanMascotaJugador = document.getElementById("mascotaJugador")
 
-    if (inputHipodoge.checked){
-        alert("Seleccionaste a Hipodoge")
-    }else if(inputCapipero.checked){
-        alert("Seleccionaste a Capipero")
-    }else if(inputRatigueya.checked){
-        alert("Seleccionaste a Ratigueya")
-    }else if(inputLangostelvis.checked){
-        alert("Seleccionaste a Langostelvis")
-    }else if(inputTucapalma.checked){
-        alert("Seleccionaste a Tucapalma")
-    }else if(inputPydos.checked){
-        alert("Seleccionaste a Pydos")
-    }else{
+    //.checked nos permite validar que nuestra etiqueta tipo input/radio este seleccionada o no.
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = "Hipodoge"
+    } else if (inputCapipero.checked) {
+        spanMascotaJugador.innerHTML = `Capipepo`
+    } else if (inputPydos.checked) {
+        spanMascotaJugador.innerHTML = `Pydos`
+        /*Inner.HTML ha sido el método que nos permitio interactuar directamente con nuestro archivo HTML, en el cual podemos
+        ahora Traer, modificar o eliminar contenido de una etiqueta*/
+    } else {
         alert("Selecciona una Mascota")
     }
+
+    seleccionarMascotaEnemigo()
+
+
+    function seleccionarMascotaEnemigo() {
+        let ataqueAleatorio = aleatorio(1, 3)
+        let spanMascotaEnemigo = document.getElementById("mascotaEnemigo")
+
+        if (ataqueAleatorio == 1) {
+            spanMascotaEnemigo.innerHTML = "Hipodoge"
+        } else if (ataqueAleatorio == 2) {
+            spanMascotaEnemigo.innerHTML = "Capipepo"
+        } else {
+            spanMascotaEnemigo.innerHTML = "Pydos"
+        }
+    }
+    
+    function aleatorio(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 }
+
 
 /**Aca estamos usando el metodo addEvenListener con el atributo no de que haga click, si no que primeramente cargue
 Esto permite que podamos crear una funsión que nos comenzará a ejecutar todo en el orden que deseamos sin la necesidad
