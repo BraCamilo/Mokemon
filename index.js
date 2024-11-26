77b6369ba3b6cce3f9b744ec32a35307db673718
BraCamilo <brayancalvo64@outlook.com>
2024-11-26 16:41:23 -0500

    Inicialización de express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hola node');
+const express = require ('express');
+
+const app = express()//Acá se genero una instacia del servidor que se estará usando
+
+app.listen(8080, () =>{
+    console.log('El servidor ya arranco')
+});
+//Hasta este momento se inicio el servidor, sin embargo no hace nada, pero ya fue indicado a nuestro nodejs que lo creara
